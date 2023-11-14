2617fdc46dc32feab1ac83a9e4587aae433e9678
F1248 <132580749+F1248@users.noreply.github.com>
2023-11-14 20:07:59 +0100

    Adjust pkg_url

--- resources/constants.py
+++ resources/constants.py
@@ -34,8 +34,8 @@ class Constants:
         self.discord_link:                    str = "https://discord.gg/rqdPgH8xSN"
         self.guide_link:                      str = "https://dortania.github.io/OpenCore-Legacy-Patcher/"
         self.api_url:                         str = "https://api.github.com/repos/repository_placeholder"
-        self.pkg_url:                         str = "https://nightly.link/repository_placeholder/workflows/build-app/branch_placeholder/AutoPkg-Assets.pkg.zip"
         self.app_url:                         str = "https://nightly.link/repository_placeholder/workflows/build-app/branch_placeholder/OpenCore%20Legacy%20Patcher.app.zip"
+        self.pkg_url:                         str = "https://nightly.link/repository_placeholder/workflows/build-package/branch_placeholder/AutoPkg-Assets.pkg.zip"
         self.support_url:                     str = "https://nightly.link/dortania/PatcherSupportPkg/workflows/build/master/Universal%20Binaries.zip"
 
         # OpenCore Versioning
