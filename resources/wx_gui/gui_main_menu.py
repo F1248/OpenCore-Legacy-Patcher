da8bb89d0cfd1110c1027b416ebeb6eb2812f1e8
F1248 <132580749+F1248@users.noreply.github.com>
2023-12-31 15:01:14 +0100

    Fix "Merge remote-tracking branch 'upstream/main'"

--- resources/wx_gui/gui_main_menu.py
+++ resources/wx_gui/gui_main_menu.py
@@ -229,7 +229,7 @@ class MainFrame(wx.Frame):
         if Path("/Applications/OpenCore Legacy Patcher.app").exists() and Path("/Applications/OpenCore Legacy Patcher.app").is_symlink() is False:
             logging.info("Found user-installed app in /Applications, replacing with symlink")
             # Delete app
-            result = subprocess.run(["/bin/ln", "-s", "/Library/Application Support/Dortania/OpenCore Legacy Patcher.app", "/Applications/OpenCore Legacy Patcher.app"], capture_output=True)
+            result = subprocess.run(["/bin/rm", "-rf", "/Applications/OpenCore Legacy Patcher.app"], capture_output=True)
             if result.returncode != 0:
                 logging.info("Failed to delete app from /Applications")
                 return
